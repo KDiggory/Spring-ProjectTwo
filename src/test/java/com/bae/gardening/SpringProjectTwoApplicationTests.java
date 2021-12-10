dec8c3a0930907aef3844cf44f23d94cffc36fef
Katie Diggory <Katie.diggory@gmail.com>
2021-12-10 19:31:00 +0000

    Creating the project in eclipse and commiting to Dev

---
+++ src/test/java/com/bae/gardening/SpringProjectTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae.gardening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
